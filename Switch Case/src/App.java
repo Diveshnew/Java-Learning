a7b77c6d411933b6b5080f0a63c5a815c2170d62
Divesh <ds7699864@gmail.com>
2023-09-20 20:04:59 +0530

    little practice today

---
+++ Switch Case/src/App.java
@@ -0,0 +1,31 @@
+public class App {
+    public static void main(String[] args){
+        // Switch Case statements
+        int num=600;
+        switch (num) {
+            case 200:
+                System.out.println("This is Case 1");
+                break;
+        
+            case 300:
+                System.out.println("This is Case 2");
+                break;
+        
+            case 400:
+                System.out.println("This is Case 3");
+                break;
+
+            case 500:
+                System.out.println("This is Case 4");
+                break;
+
+            case 600:
+                System.out.println("This is Case 5");
+                break;
+        
+            default:
+                System.out.println("This is a Default Statement");
+                break;
+        }
+    }
+}
