9280285255ea9fbb3e4a1aa295da44118bc3a4bb
Divesh <ds7699864@gmail.com>
2023-11-09 17:07:46 +0530

    9-11-23

---
+++ Arrays/src/App.java
@@ -0,0 +1,41 @@
+public class App {
+    public static void main(String[] args){
+
+
+        // ************** 1D ARRAY ***************
+
+        // int nums[] = {5,4,3,2};
+
+        // System.out.println(nums[0]);
+        // System.out.println(nums[1]);
+        // System.out.println(nums[2]);
+        // System.out.println(nums[3]);
+
+        // for(int i=0;i<=3;i++){
+        //     System.out.println(nums[i]);
+        // }
+
+        
+        //-------> When the values are unknown <-----------
+        // int nums[] = new int[4];
+        // nums[0] = 6;
+        // nums[1] = 9;
+        // nums[2] = 7;
+        // nums[3] = 4;
+
+        // for(int i=0;i<=3;i++){
+        //     System.out.println(nums[i]);
+        // }
+
+
+
+        // ------> For Strings <----------
+        String name[] = {"Naveen","Harsh","kiran"};
+        for(int i = 0; i<=2 ; i++){
+            System.out.println(name[i]);
+        }
+
+
+        
+    }
+}
--- Nested Loops and Pattern Programming/src/App.java
+++ Nested Loops and Pattern Programming/src/App.java
@@ -102,7 +102,18 @@ public class App {
             
             // This is a pattern program for a diagonal pattern
                 for(int j=0 ; j<n ; j++){
-                    if(i+j == (n-1)/2 || i-j==(n-1)/2 || j-i==(n-1)/2 || i+j==n-1 + (n-1)/2){
+                    if(i+j == (n-1)/2 || i-j==(n-1)/2 || j-i==(n-1)/2 || i+j==n-1 + (n-1)/2 || i==0 || j==0 || i==n-1 || j==n-1){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+            System.out.print("         ");
+            
+            // This is a pattern program for a diagonal pattern
+                for(int j=0 ; j<n ; j++){
+                    if(i==0 && j<=(n-1)/2 || j==0 && i<=(n-1)/2 || i+j<=(n-1)/2 || i==0 && j>=(n-1)/2 || j==n-1 && i<=(n-1)/2 || j-i>=(n-1)/2 ||  j==0 && i>=(n-1)/2 || i==n-1 && j<=(n-1)/2 || i-j>=(n-1)/2 || j==n-1 && i>=(n-1)/2 || i==n-1 && j>(n-1)/2 || i+j>=n-1 + (n-1)/2 ){
                         System.out.print("*");
                     }
                     else{
